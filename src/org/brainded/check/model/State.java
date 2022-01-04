b24fa5657d6571f38938f6623c0ca004bad26415
Pierre Lemaigre <pierrelemaigre.pro@gmail.com>
2022-01-04 15:53:46 +0100

    Kripke Structure Defined

---
+++ src/org/brainded/check/model/State.java
@@ -0,0 +1,54 @@
+package org.brainded.check.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class State {
+    private final boolean initialState;
+    private final String stateName;
+    private final List<State> successors;
+    private final List<String> labels;
+
+    State(boolean initialState, String stateName) {
+       this.initialState = initialState;
+       this.stateName = stateName;
+       this.successors = new ArrayList<>();
+       this.labels = new ArrayList<>();
+    }
+
+    public boolean isInitialState() {
+        return initialState;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void addSuccesor(State successor) {
+        if (!successors.contains(successor)) {
+           addSuccesor(successor);
+        }
+    }
+
+    public List<State> getSuccessors() {
+        return successors;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return stateName.equals(state.stateName);
+    }
+
+    @Override
+    public int hashCode() {
+        return stateName.hashCode();
+    }
+}
