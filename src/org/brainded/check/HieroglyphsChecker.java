33935f650deb84db4ef0e491c41b98682a790e64
Arnaud Saulou <a.saulou@gmail.com>
2022-01-05 09:41:34 +0100

    Menu: - Stacked menu

--- src/org/brainded/check/HieroglyphsChecker.java
+++ src/org/brainded/check/HieroglyphsChecker.java
@@ -1,8 +1,12 @@
 package org.brainded.check;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HieroglyphsChecker {
+
+    //region Variables
 
     private enum Action {
         LOAD_KRIPKE,
@@ -11,11 +15,22 @@ public class HieroglyphsChecker {
         DEFAULT
     }
 
-    private static final Scanner keyboard = new Scanner(System.in);
+    private static final BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    //endregion
+
+    //region Menu
+
+    private static void act() {
+        switch (displayMenu()) {
+            case LOAD_KRIPKE -> loadKripke();
+            case ENTER_CTL -> enterCtl();
+            case QUIT -> quit();
+        }
+    }
 
     private static Action displayMenu() {
-        Action actionAsked;
-        int answer = -1;
+        Action actionAsked = Action.DEFAULT;
 
         do {
             System.out.println("\n-- Hieroglyphs checker menu -- \n");
@@ -23,54 +38,92 @@ public class HieroglyphsChecker {
             System.out.println("2. Enter CTL state formulae");
             System.out.println("3. Quit");
             System.out.print("Enter your choice : ");
-            answer = keyboard.nextInt();
+            actionAsked = getActionAsked(actionAsked);
             System.out.println("------------------------------");
-
-            actionAsked = switch (answer) {
-                case 1 -> Action.LOAD_KRIPKE;
-                case 2 -> Action.ENTER_CTL;
-                case 3 -> Action.QUIT;
-                default -> Action.DEFAULT;
-            };
-
         } while (actionAsked == Action.DEFAULT);
 
         return actionAsked;
     }
 
-    private static void loadKripke(){
+    private static Action getActionAsked(Action actionAsked) {
+        Integer answer;
+        answer = readIntInput();
+
+        if (answer != null) {
+            switch (answer) {
+                case 1 -> actionAsked = Action.LOAD_KRIPKE;
+                case 2 -> actionAsked = Action.ENTER_CTL;
+                case 3 -> actionAsked = Action.QUIT;
+                default -> printError("Invalid choice");
+            }
+        }
+        return actionAsked;
+    }
+
+    //endregion
+
+    //region Actions
+    private static void loadKripke() {
         System.out.println("\n-- Load Kripke structure file -- \n");
         System.out.print("Enter the path to the Kripke structure file : ");
 
-        String kripkeFilePath = keyboard.next();
-
+        String kripkeFilePath = readStringInput();
         System.out.println(kripkeFilePath);
 
         act();
     }
 
-    private static void enterCtl(){
+    private static void enterCtl() {
         System.out.println("\n-- Enter CTL state formulae -- \n");
         System.out.print("Enter the CTL state formulae to check : ");
 
-        String ctlFormulae = keyboard.next();
-
+        String ctlFormulae = readStringInput();
         System.out.println(ctlFormulae);
 
         act();
     }
 
-    private static void quit(){
+    private static void quit() {
         System.out.println("\nBye !");
     }
 
-    private static void act(){
-        switch (displayMenu()){
-            case LOAD_KRIPKE -> loadKripke();
-            case ENTER_CTL -> enterCtl();
-            case QUIT -> quit();
+    //endregion
+
+    //region Utils
+
+    //region Input
+
+    private static Integer readIntInput() {
+        Integer intInput = null;
+        try {
+            intInput = Integer.parseInt(keyboard.readLine());
+        } catch (NumberFormatException | IOException e) {
+            printError("Please enter en number");
         }
+        return intInput;
     }
+
+    private static String readStringInput() {
+        String stringInput = null;
+        try {
+            stringInput = keyboard.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringInput;
+    }
+
+    //endregion
+
+    //region Output
+
+    private static void printError(String errorMsg) {
+        System.out.println("\u001B[31m" + errorMsg + "\u001B[0m");
+    }
+
+    //endregion
+
+    //endregion
 
     public static void main(String[] args) {
         act();
