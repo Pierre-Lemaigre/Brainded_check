c3276937952d76e3d9c70547c8383cd12d7598b3
Pierre Lemaigre <pierrelemaigre.pro@gmail.com>
2022-01-10 10:54:14 +0100

    CTL func on 3 algo

--- src/org/brainded/check/Checker.java
+++ src/org/brainded/check/Checker.java
@@ -5,40 +5,52 @@ import org.brainded.check.model.State;
 import org.brainded.check.model.ctl.Atom;
 import org.brainded.check.model.ctl.Operand;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Checker {
 
-    private KripkeStructure kripkeStructure;
+    private final KripkeStructure kripkeStructure;
     private List<Operand> ctrlFormulae;
-    private List<State> validatingStates;
+    private final Set<State> validatingStates;
 
     public Checker(KripkeStructure kripkeStructure, List<Operand> ctrlFormulae){
         this.kripkeStructure = kripkeStructure;
         this.ctrlFormulae = ctrlFormulae;
-        this.validatingStates = new ArrayList<>();
+        this.validatingStates = new HashSet<>();
     }
 
-    public List<State> getValidatingStates() {
+    public Set<State> getValidatingStates() {
         return this.validatingStates;
     }
 
-    public void marking(Operand operand){
-        if(operand instanceof Atom){
-            for (State state: this.kripkeStructure.getStates()) {
-                if(state.getLabels().contains(((Atom) operand).getAtomicName()))
-                    this.validatingStates.add(state);
-            }
+    public Set<State> marking(Atom proposition){
+        Set<State> marked = new HashSet<>();
+        for (State state: this.kripkeStructure.getStates()) {
+            if(state.getLabels().contains(proposition.getAtomicName()))
+                marked.add(state);
         }
+        return marked;
     }
 
+    public Set<State> reverseMarking(Atom proposition) {
+        Set<State> marked = new HashSet<>();
+        for (State state: this.kripkeStructure.getStates()) {
+            if(!state.getLabels().contains(proposition.getAtomicName()))
+                marked.add(state);
+        }
+        return marked;
+    }
 
-
-
-
-
-
-
-
+    public Set<State> AND(Atom p1, Atom p2) {
+        Set<State> marked = new HashSet<>();
+        for (State state: this.kripkeStructure.getStates()) {
+            if (state.getLabels().contains(p1.getAtomicName()) &&
+                state.getLabels().contains(p2.getAtomicName())) {
+                marked.add(state);
+            }
+        }
+        return marked;
+    }
 }
